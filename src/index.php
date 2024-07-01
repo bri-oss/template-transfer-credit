731d9ef7787e6b7e4bed528a2990368a723fbff4
AlfaRiza <malfariza45@gmail.com>
2024-07-01 10:01:05 +0700

    feat: split interbank, intrabank and transaction status inquiry

--- src/index.php
+++ src/index.php
@@ -1,15 +1,9 @@
 <?php
-
-include 'util.php';
 require __DIR__ . '/../vendor/autoload.php';
 
 Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..' . '')->load();
 
 require __DIR__ . '/../briapi-sdk/autoload.php';
-use BRI\Balance\Balance;
-use BRI\Token\AccessToken;
-use BRI\Util\VarNumber;
-use BRI\Signature\Signature;
 
 // env values
 $clientId = $_ENV['CONSUMER_KEY']; // customer key
@@ -22,41 +16,5 @@ $path = '/snap/v1.0/balance-inquiry'; //informasi rekening api path
 $accessTokenPath = '/snap/v1.0/access-token/b2b'; //access token path
 
 // change variables accordingly
-$account = '111231271284142'; // account number
-$partnerId = 'feedloop'; //partner id
-$channelId = '12345'; // channel id
-
-//external id
-$externalId = (new VarNumber())->generateVar(9);
-
-// fetches a new access token every specified minute with a maximum of 15 minutes
-$minutes = 15;
-
-if (!file_exists('accessToken.txt') || isTokenExpired('timestamp.txt', $minutes)) {
-  //timestamp
-  $timestamp = (new DateTime('now', new DateTimeZone('Asia/Jakarta')))->format('Y-m-d\TH:i:s.000P');
-
-  //access token
-  $accessToken = (new AccessToken(new Signature()))->getAccessToken(
-    $clientId,
-    $pKeyId,
-    $timestamp,
-    $baseUrl,
-    $accessTokenPath,
-  );
-
-  file_put_contents('accessToken.txt', $accessToken);
-  file_put_contents('timestamp.txt', $timestamp);
-
-  echo "New Token is created\n";
-} else {
-  $accessToken = trim(file_get_contents('accessToken.txt'));
-  $timestamp = trim(file_get_contents('timestamp.txt'));
-  echo "Used Token\n";
-}
-
-$response = (new Balance())->inquiry($account, $clientSecret, $partnerId, $baseUrl, $path, $accessToken, $channelId, $externalId, $timestamp);
-
-$json = json_encode($response, JSON_PRETTY_PRINT);
-
-echo $response;
+$partnerId = ''; //partner id
+$channelId = ''; // channel id
